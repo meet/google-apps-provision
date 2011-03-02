6e61c7d499e38dd413834eaa697d57bab4c88f99
Max Goldman <maxg@meet.mit.edu>
2011-03-02 16:18:00 -0500

    User passwords

--- lib/google-apps-provision/user.rb
+++ lib/google-apps-provision/user.rb
@@ -8,11 +8,18 @@ module GoogleApps
     post_new { "#{ENDPOINT}/#{GoogleApps.connection.domain}/user/2.0" }
     put_updated { |u| "#{ENDPOINT}/#{GoogleApps.connection.domain}/user/2.0/#{u.user_name}" }
     
-    attributes 'apps:login', :user_name, :admin, :suspended, :agreed_to_terms, :change_password_at_next_login, :ip_whitelisted
+    attributes 'apps:login', :user_name, :admin, :suspended, :agreed_to_terms,
+                             :password, :hash_function_name, :change_password_at_next_login, :ip_whitelisted
     attributes 'apps:name', :given_name, :family_name
     attributes 'apps:quota', :limit
     
     identity :user_name
+    
+    # Set password by sending hashed value.
+    def new_password=(value)
+      @password = Digest::SHA1.hexdigest(value)
+      @hash_function_name = 'SHA-1'
+    end
     
   end
   
--- test/user_test.rb
+++ test/user_test.rb
@@ -20,4 +20,14 @@ class UserTest < Test::Unit::TestCase
     assert sergey.agreed_to_terms
   end
   
+  def test_set_password
+    GoogleApps.connection.mock(:GET,
+                               'https://apps-apis.google.com/a/feeds/example.com/user/2.0/sergey',
+                               fixture(:sergey))
+    sergey = GoogleApps::User.find('sergey')
+    sergey.new_password = 'secret!'
+    assert_equal Digest::SHA1.hexdigest('secret!'), sergey.password
+    assert_equal 'SHA-1', sergey.hash_function_name
+  end
+  
 end
